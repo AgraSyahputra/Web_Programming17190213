240c8444675c8e1c7267234297625b39fc23f67c
AgraSyahputra <putrailham160400@gmail.com>
2022-05-19 16:42:48 +0700

    first commit

---
+++ application/controllers/contoh_1.php
@@ -0,0 +1,16 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "Biodata Saya<br>";
+		echo "Nama Lengkap : Agra Syahputra<br>";
+		echo "Tempat/Tanggal Lahir : Bekasi, 16 April 2000<br>";
+		echo "Umur : 22 Tahun<br>";
+		echo "Nim : 17190213<br>";
+		echo "Status : Mahasiswa Aktif Semester 6<br>";
+		echo "Fakultas : Teknik dan Informatika<br>";
+		echo "Jurusan : Teknologi Informasi<br>";
+		echo "Jenis Kelamin : Laki-Laki";
+	}
+}
